9c14271eda1a9636cb0bbf2e326781896c92f2b6
tbtanhhh <155649760+ieathn@users.noreply.github.com>
2025-12-05 23:05:05 +0700

    Update library_fine.py

--- borrowing_book/models/library_fine.py
+++ borrowing_book/models/library_fine.py
@@ -1,7 +1,4 @@
-# -*- coding: utf-8 -*-
 from odoo import models, fields, api
-
-
 class LibraryFine(models.Model):
     _name = 'library.fine'
     _description = 'Library Fine'
@@ -10,40 +7,33 @@ class LibraryFine(models.Model):
     name = fields.Char(
         string='Fine Code',
         default=lambda self: self.env['ir.sequence'].next_by_code('library.fine'),
-        readonly=True
-    )
+        readonly=True)
 
     transaction_id = fields.Many2one(
         'library.transaction',
         string='Transaction',
         required=True,
-        tracking=True
-    )
+        tracking=True,
+        ondelete='cascade',  )
 
     member_id = fields.Many2one(
         'library.member',
         string='Member',
         related='transaction_id.member_id',
-        store=True
-    )
+        store=True)
 
     book_copy_id = fields.Many2one(
         'library.book.copy',
         string='Book Copy',
         related='transaction_id.book_copy_id',
-        store=True
-    )
+        store=True)
 
     amount = fields.Float(
         string='Amount',
-        default=0.0,
-        tracking=True
-    )
+        related='transaction_id.fine_amount',
+        store=True,
+        readonly=False,   
+        tracking=True)
 
     paid = fields.Boolean(string='Paid', default=False)
 
-    @api.onchange('transaction_id')
-    def _onchange_transaction_id(self):
-        for rec in self:
-            if rec.transaction_id:
-                rec.amount = rec.transaction_id.fine_amount
